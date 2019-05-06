3c064131755ae0160605f3a20d1aed13cbf7804f
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-05-06 11:22:34 +0100

    Add files via upload

---
+++ src/layers.py
@@ -0,0 +1,140 @@
+import math
+import torch
+from torch_sparse import spmm
+
+def uniform(size, tensor):
+    """
+    Uniform weight initialization.
+    :param size: Size of the tensor.
+    :param tensor: Tensor initialized.
+    """
+    stdv = 1.0 / math.sqrt(size)
+    if tensor is not None:
+        tensor.data.uniform_(-stdv, stdv)
+
+class SparseNGCNLayer(torch.nn.Module):
+    """
+    Multi-scale GCN layer.
+    :param in_channels: Number of features.
+    :param out_channels: Number of filters.
+    :param iterations: Adjacency matrix power order.
+    :param dropout_rate: Dropout value.
+    """
+    def __init__(self, in_channels, out_channels, iterations, dropout_rate):
+        super(SparseNGCNLayer, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.iterations = iterations
+        self.dropout_rate = dropout_rate
+        self.define_parameters()
+        self.init_parameters()
+
+    def define_parameters(self):
+        """
+        Defining the weight matrices.
+        """
+        self.weight_matrix = torch.nn.Parameter(torch.Tensor(self.in_channels, self.out_channels))
+        self.bias = torch.nn.Parameter(torch.Tensor(self.out_channels))
+
+    def init_parameters(self):
+        """
+        Initializing weights.
+        """
+        torch.nn.init.xavier_uniform_(self.weight_matrix)
+        uniform(self.out_channels,self.bias)
+
+    def forward(self, normalized_adjacency_matrix, features):
+        """
+        Doing a forward pass.
+        :param normalized_adjacency_matrix: Normalized adjacency matrix.
+        :param features: Feature matrix.
+        :return base_features: Convolved features.
+        """
+        base_features = spmm(features["indices"], features["values"], features["dimensions"][0],  self.weight_matrix)
+        base_features = torch.nn.functional.dropout(base_features, p = self.dropout_rate, training = self.training)
+        base_features = torch.nn.functional.relu(base_features) + self.bias
+        for iteration in range(self.iterations):
+            base_features = spmm(normalized_adjacency_matrix["indices"], normalized_adjacency_matrix["values"], base_features.shape[0], base_features)
+        return base_features
+
+class DenseNGCNLayer(torch.nn.Module):
+    """
+    Multi-scale GCN layer.
+    :param in_channels: Number of features.
+    :param out_channels: Number of filters.
+    :param iterations: Adjacency matrix power order.
+    :param dropout_rate: Dropout value.
+    """
+    def __init__(self, in_channels, out_channels, iterations, dropout_rate):
+        super(DenseNGCNLayer, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.iterations = iterations
+        self.dropout_rate = dropout_rate
+        self.define_parameters()
+        self.init_parameters()
+
+    def define_parameters(self):
+        """
+        Defining the weight matrices.
+        """
+        self.weight_matrix = torch.nn.Parameter(torch.Tensor(self.in_channels, self.out_channels))
+        self.bias = torch.nn.Parameter(torch.Tensor(self.out_channels))
+
+    def init_parameters(self):
+        """
+        Initializing weights.
+        """
+        torch.nn.init.xavier_uniform_(self.weight_matrix)
+        uniform(self.out_channels,self.bias)
+
+    def forward(self, normalized_adjacency_matrix, features):
+        """
+        Doing a forward pass.
+        :param normalized_adjacency_matrix: Normalized adjacency matrix.
+        :param features: Feature matrix.
+        :return base_features: Convolved features.
+        """
+        base_features = torch.mm(features,  self.weight_matrix)
+        base_features = torch.nn.functional.dropout(base_features, p = self.dropout_rate, training = self.training)
+        base_features = torch.nn.functional.relu(base_features) + self.bias
+        for iteration in range(self.iterations):
+            base_features = spmm(normalized_adjacency_matrix["indices"], normalized_adjacency_matrix["values"], base_features.shape[0], base_features)
+        return base_features
+
+class ListModule(torch.nn.Module):
+    """
+    Abstract list layer class.
+    """
+    def __init__(self, *args):
+        """
+        Module initializing.
+        """
+        super(ListModule, self).__init__()
+        idx = 0
+        for module in args:
+            self.add_module(str(idx), module)
+            idx += 1
+
+    def __getitem__(self, idx):
+        """
+        Getting the indexed layer.
+        """
+        if idx < 0 or idx >= len(self._modules):
+            raise IndexError('index {} is out of range'.format(idx))
+        it = iter(self._modules.values())
+        for i in range(idx):
+            next(it)
+        return next(it)
+
+    def __iter__(self):
+        """
+        Iterating on the layers.
+        """
+        return iter(self._modules.values())
+
+    def __len__(self):
+        """
+        Number of layers.
+        """
+        return len(self._modules)
